8aa9e64f1b83c917745e76f96eb91d7ae3ad9b3b
sassinzz13 <diamondassasinzz@gmail.com>
2025-11-05 16:20:08 +0800

    added comments for guide

--- server/products/urls.py
+++ server/products/urls.py
@@ -4,9 +4,12 @@ from .views import ProductList, CreateProduct, EditProduct, DeleteProduct, Produ
 urlpatterns = [
     # http://127.0.0.1:8000/shop/products/
     path('products/', ProductList.as_view(), name='product-list'),
-    path('products/', ProductList.as_view(), name='product-list-id'),
+    # http://127.0.0.1:8000/shop/products/1/
     path('products/<int:pk>/', ProductDetail.as_view(), name='product-detail'),
+    # http://127.0.0.1:8000/shop/create/
     path('create/', CreateProduct.as_view(), name='product-create'),
+    # http://127.0.0.1:8000/shop/products/1/edit/
     path('products/<int:pk>/edit/', EditProduct.as_view(), name='product-edit'),
+    # http://127.0.0.1:8000/shop/products/1/delete/
     path('products/<int:pk>/delete/', DeleteProduct.as_view(), name='product-delete')
 ]
