802b53591fa8241b3ce21359fa4a4221298c4727
its_sassinzz13 <66528388+sassinzz13@users.noreply.github.com>
2025-11-11 18:48:58 +0800

    added environs for cloudinary security

--- server/core/settings.py
+++ server/core/settings.py
@@ -57,10 +57,12 @@ import cloudinary.uploader
 import cloudinary.api
 
 CLOUDINARY_STORAGE = {
-    'CLOUD_NAME': 'dwdhdzva8',
-    'API_KEY': '519217737817169',
-    'API_SECRET': '-cIC2J877nmuILeNb25SIFCY8AA',
+    'CLOUD_NAME': env('CLOUDINARY_CLOUD_NAME'),
+    'API_KEY': env('CLOUDINARY_API_KEY'),
+    'API_SECRET': env('CLOUDINARY_API_SECRET'),
 }
+
+DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
